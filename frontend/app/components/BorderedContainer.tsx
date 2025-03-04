36a45e4e65945bccd5082f4e26c057c760ea5a7c
nsfisis <nsfisis@gmail.com>
2025-03-04 22:01:37 +0900

    update index page

--- frontend/app/components/BorderedContainer.tsx
+++ frontend/app/components/BorderedContainer.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function BorderedContainer({ children }: Props) {
 	return (
-		<div className="bg-white border-2 border-pink-600 rounded-xl p-4">
+		<div className="bg-white border-2 border-blue-600 rounded-xl p-4">
 			{children}
 		</div>
 	);
--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -14,27 +14,26 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function Index() {
 	return (
-		<div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center gap-y-6">
+		<div className="min-h-screen bg-sky-600 flex flex-col items-center justify-center gap-y-6">
 			<img
-				src="/phperkaigi/2025/code-battle/favicon.svg"
-				alt="iOSDC Japan 2024"
-				className="w-24 h-24"
+				src="/phperkaigi/2025/code-battle/logo.svg"
+				alt="PHPerKaigi 2025"
+				className="w-64 h-64"
 			/>
 			<div className="text-center">
-				<div className="font-bold text-transparent bg-clip-text bg-iosdc-japan flex flex-col gap-y-2">
-					<div className="text-3xl">iOSDC Japan 2024</div>
-					<div className="text-6xl">Swift Code Battle</div>
+				<div className="font-bold text-transparent text-sky-50 flex flex-col gap-y-2">
+					<div className="text-5xl">PHPER CODE BATTLE</div>
 				</div>
 			</div>
 			<div className="mx-2">
 				<BorderedContainer>
 					<p className="text-gray-900 max-w-prose">
-						Swift コードバトルは指示された動作をする Swift
+						PHPer コードバトルは指示された動作をする PHP
 						コードをより短く書けた方が勝ち、という 1 対 1
-						の対戦コンテンツです。8/22（木）day0 前夜祭では 8/12
+						の対戦コンテンツです。3/21（金）day0 前夜祭では 3/8
 						に実施された予選を勝ち抜いたプレイヤーによるトーナメント形式での
-						Swift
-						コードバトルを実施します。ここでは短いコードが正義です！可読性も保守性も放り投げた、イベントならではのコードをお楽しみください！
+						PHPer コードバトルを実施します。
+						ここでは短いコードが正義です！可読性も保守性も放り投げた、イベントならではのコードをお楽しみください！
 					</p>
 				</BorderedContainer>
 			</div>
