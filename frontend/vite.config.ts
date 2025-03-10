dfab679672146d60fdbffab7a97d9a9fc6ed58b3
nsfisis <nsfisis@gmail.com>
2025-03-11 00:08:45 +0900

    fix(frontend): fix path of public files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+	base: "/phperkaigi/2025/code-battle/",
 	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
