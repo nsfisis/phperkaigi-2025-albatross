c2e23841f446489c4d9b7d2b616f3de249159f5f
nsfisis <nsfisis@gmail.com>
2025-03-15 22:55:04 +0900

    feat(frontend): show notice for players

--- frontend/app/components/Gaming/Problem.tsx
+++ frontend/app/components/Gaming/Problem.tsx
@@ -19,6 +19,30 @@ export default function Problem({ title, description, sampleCode }: Props) {
 			<BorderedContainerWithCaption caption="サンプルコード">
 				<CodeBlock code={sampleCode} language="php" />
 			</BorderedContainerWithCaption>
+			<BorderedContainerWithCaption caption="スコア計算・PHP 環境">
+				<div className="text-gray-700 flex flex-col gap-2">
+					<p>スコアはコード中の全 ASCII 空白文字を除去した後のバイト数です。</p>
+					<p>
+						この環境の PHP バージョンは{" "}
+						<strong className="font-bold">8.4.4</strong> です。 mbstring
+						を除くほとんどの拡張は無効化されています。
+						また、ファイルやネットワークアクセスはできません。
+					</p>
+					<p>
+						テストの成否は、標準出力へ出力された文字列を比較して判定されます。
+						末尾の改行はあってもなくても構いません。
+						標準エラー出力の内容は無視されますが、fatal error
+						等で実行が中断された場合は失敗扱いとなります。
+					</p>
+					<p>
+						なお、<code>error_reporting</code> は{" "}
+						<code>
+							E_ALL &amp; ~E_WARNING &amp; ~E_NOTICE &amp; ~E_DEPRECATED
+						</code>{" "}
+						に設定されています。
+					</p>
+				</div>
+			</BorderedContainerWithCaption>
 		</div>
 	);
 }
