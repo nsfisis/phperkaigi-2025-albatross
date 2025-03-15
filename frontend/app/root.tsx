b933bed3b6bf8b51b2916c80533ed64afd27f653
nsfisis <nsfisis@gmail.com>
2025-03-15 21:44:39 +0900

    feat(frontend): add PHPer token

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -24,6 +24,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				{children}
 				<ScrollRestoration />
 				<Scripts />
+				<script>console.log(`#Albatross!`)</script>
 			</body>
 		</html>
 	);
